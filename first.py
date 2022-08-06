33ab9fbd48a3c6de5d7c700e7af30c058bc82ddc
sanket-babar <sanketbabar57@gmail.com>
2022-08-07 01:10:50 +0530

    Extracting title

--- first.py
+++ first.py
@@ -1,8 +1,12 @@
 import requests
-
+from bs4 import BeautifulSoup
 res = requests.get(
     'https://codedamn-classrooms.github.io/webscraper-python-codedamn-classroom-website/')
 txt = res.text
 status = res.status_code
 
-print(txt, status)
+soup = BeautifulSoup(txt, 'html.parser')
+
+page_title = soup.title.string
+
+print(page_title)
