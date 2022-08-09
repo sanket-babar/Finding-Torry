1200c5619e6965acb8177a20405963571e275ced
thunderfreak <sohambhattacharjee0711@gmail.com>
2022-08-09 22:02:38 +0530

    output embeds in cards

--- bot_online.py
+++ bot_online.py
@@ -34,15 +34,16 @@ async def on_message(message):
       for link in magnet_links:
         await message.channel.send(link)
     else:
-      await message.channel.send(no_result_message)  
+      await message.channel.send(no_result_message)
+
   if f'?find' in message_content:
     key_words, search_words = search_web.key_words_search_words(message_content)
     result_links = search_web.search(key_words)
     links = search_web.send_link(result_links, search_words)
     magnet_links = search_web.magnet(links)
     for link in magnet_links:
-      embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
-      embedVar.add_field(f"Movie title [Movie name]({link})")
+      embedVar = discord.Embed(title="Title", url='https://stackoverflow.com/questions/63160401/how-can-a-discord-bot-create-a-hyperlink-in-a-discord-message-in-an-embed-or-in', description="Desc", color=0x00ff00)
+      # embedVar.add_field(f"Movie title [Movie name]({link})")
       await message.channel.send(embed=embedVar)    
 
 # gets bot token
